500589f8bb3fb6c99319b4e5fd7dbf5c38f3baf3
Dave Ebbelaar <dave@datalumina.com>
2024-10-25 16:11:20 +0200

    checkpoint

--- playground/prompts.py
+++ playground/prompts.py
@@ -5,13 +5,20 @@ sys.path.append("..")
 from services.prompt_loader import PromptManager
 
 
+# --------------------------------------------------------------
+# Test support prompt
+# --------------------------------------------------------------
+
 support_prompt = PromptManager.get_prompt(
     "ticket_analysis", pipeline="support", ticket={}
 )
+print(support_prompt)
+
+# --------------------------------------------------------------
+# Test helpdesk prompt
+# --------------------------------------------------------------
 
 helpdesk_prompt = PromptManager.get_prompt(
     "ticket_analysis", pipeline="helpdesk", ticket={}
 )
-
-print(support_prompt)
 print(helpdesk_prompt)
--- playground/rag.py
+++ playground/rag.py
@@ -1,7 +1,20 @@
+import os
+
+# Set database host to localhost since we're connecting to it outside of docker
+os.environ["DATABASE_HOST"] = "localhost"
 from services.vector_store import VectorStore
 
 
 vec = VectorStore()
 
-vec.semantic_search("What's the working from home policy?")
-vec.keyword_search("Policy")
+# --------------------------------------------------------------
+# Test semantic search
+# --------------------------------------------------------------
+
+result = vec.semantic_search("What's the working from home policy?")
+
+# --------------------------------------------------------------
+# Test keyword search
+# --------------------------------------------------------------
+
+result = vec.keyword_search("Policy")

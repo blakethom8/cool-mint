ce03bb23ad9fd83faa1df99c905de82d2642992e
Joris Ebbelaar <info@jorisebbelaar.nl>
2025-04-29 10:50:19 +0200

    Remove comment

--- playground/workflow_playground.py
+++ playground/workflow_playground.py
@@ -15,10 +15,6 @@ from workflows.workflow_registry import WorkflowRegistry
 This playground is used to test the WorkflowRegistry and the workflows themselves.
 """
 
-# --------------------------------------------------------------
-# Test invoice event (customer workflow)
-# --------------------------------------------------------------
-
 event = EventLoader.load_event(event_key="placeholder_event")
 workflow = WorkflowRegistry.PLACEHOLDER.value()
 output = workflow.run(event)
